89954b1c8baeea0e2a83fe657f48abf3879a7e48
Mcen25 <matthew.en.c@gmail.com>
2024-02-02 17:09:29 -0500

    steamworks and heathen installation

---
+++ Project Survive/Assets/_Heathen Engineering/Assets/com.heathen.steamworkscomplete/Samples/Examples/11 Steam Game Server/Game Server Browser/GameServerBrowser_EntryUI.cs
@@ -0,0 +1,40 @@
+﻿#if !DISABLESTEAMWORKS && HE_SYSCORE && STEAMWORKSNET
+
+using UnityEngine;
+
+namespace HeathenEngineering.DEMO
+{
+    public class GameServerBrowser_EntryUI : MonoBehaviour
+    {
+        [SerializeField]
+        private TMPro.TextMeshProUGUI displayName;
+        [SerializeField]
+        private TMPro.TextMeshProUGUI players;
+        [SerializeField]
+        private TMPro.TextMeshProUGUI ping;
+        [SerializeField]
+        private TMPro.TextMeshProUGUI steamId;
+
+        public string Name
+        {
+            get => displayName.text;
+            set => displayName.text = value;
+        }
+        public string Players
+        {
+            get => players.text;
+            set => players.text = value;
+        }
+        public string Ping
+        {
+            get => ping.text;
+            set => ping.text = value;
+        }
+        public string SteamId
+        {
+            get => steamId.text;
+            set => steamId.text = value;
+        }
+    }
+}
+#endif
---
+++ Project Survive/Assets/_Heathen Engineering/Assets/com.heathen.steamworkscomplete/Samples/Examples/7 Workshop/Scene7Behaviour.cs
@@ -0,0 +1,106 @@
+﻿#if HE_SYSCORE && (STEAMWORKSNET || FACEPUNCH) && !DISABLESTEAMWORKS 
+using UnityEngine;
+using UGC = HeathenEngineering.SteamworksIntegration.API.UserGeneratedContent.Client;
+using HeathenEngineering.SteamworksIntegration;
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace HeathenEngineering.DEMO
+{
+    /// <summary>
+    /// This is for demonstration purposes only
+    /// </summary>
+    [System.Obsolete("This script is for demonstration purposes ONLY")]
+    public class Scene7Behaviour : MonoBehaviour
+    {
+        public UserGeneratedContentQueryManager queryManager;
+        public UnityEngine.UI.Text pageCount;
+        public UnityEngine.UI.InputField searchInput;
+        public GameObject recordTemplate;
+        public Transform contentRoot;
+
+        private List<GameObject> currentRecords = new List<GameObject>();
+
+        public void QueryItems()
+        {
+            queryManager.SearchAll(searchInput.text);
+        }
+
+        public void UpdateResults(List<WorkshopItem> results)
+        {
+            pageCount.text = queryManager.activeQuery.Page.ToString() + " of " + queryManager.activeQuery.pageCount.ToString();
+
+            while(currentRecords.Count > 0)
+            {
+                var target = currentRecords[0];
+                currentRecords.Remove(target);
+                Destroy(target);
+            }
+
+            foreach(var result in results)
+            {
+                var go = Instantiate(recordTemplate, contentRoot);
+                currentRecords.Add(go);
+                var comp = go.GetComponent<Scene7DisplayItem>();
+                comp.AssignResult(result);
+            }
+        }
+
+        public void ListSubscribedItems()
+        {
+            var items = UGC.GetSubscribedItems();
+            if(items != null)
+            {
+                Debug.Log("Found " + items.Length + " items.");
+            }
+            else
+                Debug.Log("Found 0 items.");
+        }
+
+        [ContextMenu("Create Item Example")]
+        public void CreatItemExample()
+        {
+            var itemData = new WorkshopItemData
+            {
+                title = "A new title",
+                description = "A new description",
+                preview = new System.IO.FileInfo("D:\\UGCTest\\testPreviewImage.jpg"),
+                content = new System.IO.DirectoryInfo("D:\\UGCTest\\testUGCFolder"),
+                appId = AppData.Me,
+                visibility = Steamworks.ERemoteStoragePublishedFileVisibility.k_ERemoteStoragePublishedFileVisibilityUnlisted
+            };
+            itemData.Create(completedCallback =>
+            {
+                if(completedCallback.hasError)
+                {
+                    if(completedCallback.submitItemUpdateResult.HasValue)
+                    {
+                        Debug.Log("Completed Callback was invoked\nError on Item Update: " + completedCallback.errorMessage);
+                    }
+                    else if (completedCallback.createItemResult.HasValue) 
+                    {
+                        Debug.Log("Completed Callback was invoked\nError on Item Create: " + completedCallback.errorMessage);
+                    }
+                    else
+                    {
+                        Debug.Log("Completed Callback was invoked\nIO Error: " + completedCallback.errorMessage);
+                    }
+                }
+                else
+                {
+                    Debug.Log("Completed Callback was invoked");
+                }
+            },
+            uploadStartedCallback =>
+            {
+                Debug.Log("Upload Started Callback was invoked");
+            },
+            fileCreatedCallback =>
+            {
+                Debug.Log("File Created Callback was invoked");
+            });
+        }
+    }
+}
+#endif
