8d785af56d3740e6c12e8ddf138bab89f5da7421
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-05-21 17:45:04 -0400

    Sync observer and button press timer

---
+++ Project Survive/Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Connection;
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using System.Threading;
+
+public class ButtonPressed : NetworkBehaviour
+{
+    [SyncVar] public float timer = 10f;
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(StartCountdown());
+    }
+
+    private IEnumerator StartCountdown()
+    {
+        while (timer > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timer--;
+        }
+    }
+}
