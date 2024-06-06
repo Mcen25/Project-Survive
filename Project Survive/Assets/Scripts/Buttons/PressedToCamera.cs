eb2829d4d3f8c9a08db9d4050c70d2ab38d7b32a
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-06-06 18:03:36 -0400

    Changing Camera Work

--- Project Survive/Assets/Scripts/Buttons/PressedToCamera.cs
+++ Project Survive/Assets/Scripts/Buttons/PressedToCamera.cs
@@ -3,36 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
 
-public class PressedToCamera : NetworkBehaviour
+public class PressedToCamera : MonoBehaviour
 {
 
-    [SerializeField] private Camera firstPersonCamera;
-    [SerializeField] private Camera overheadCamera;
+    public Camera firstPersonCamera;
+    public Camera overheadCamera;
     
-    public void ShowFirstPersonView() {
-        firstPersonCamera.enabled = true;
-        overheadCamera.enabled = false;
-    }
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-        if (!base.IsOwner)
-        {
-            GetComponent<PressedToCamera>().enabled = false;
-        }
+    // public void ShowFirstPersonView() {
+    //     firstPersonCamera.enabled = true;
+    //     overheadCamera.enabled = false;
+    // }
+    // public override void OnStartClient()
+    // {
+    //     base.OnStartClient();
+    //     if (!base.IsOwner)
+    //     {
+    //         GetComponent<PressedToCamera>().enabled = false;
+    //     }
         
-    }
+    // }
 
-     private void OnMouseDown()
-    {
+    //  private void OnMouseDown()
+    // {
     
-        // GameObject clickedObject = GameObject.Find(hit.transform.name);
-        // Debug.Log("Clicked object: " + clickedObject.name);
+    //     // GameObject clickedObject = GameObject.Find(hit.transform.name);
+    //     // Debug.Log("Clicked object: " + clickedObject.name);
    
-    }
+    // }
 
-    private void ShowOverheadView() {
-        firstPersonCamera.enabled = false;
-        overheadCamera.enabled = true;
-    }
+    // public void ShowOverheadView() {
+    //     firstPersonCamera.enabled = false;
+    //     overheadCamera.enabled = true;
+    // }
 }
--- Project Survive/Assets/Scripts/Player Scripts/OutlineSelection.cs
+++ Project Survive/Assets/Scripts/Player Scripts/OutlineSelection.cs
@@ -11,65 +11,74 @@ public class OutlineSelection : MonoBehaviour
 
     public float maxDistance = 5f;
     private Transform previousHighlight = null;
+    
+    [SerializeField] private Camera fpsCamera;
+    [SerializeField] private Camera currentCamera;
 
     void Update()
     {  
 
-    ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+    ray = currentCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
 
-        if (Physics.Raycast(ray, out hit, maxDistance))
-        {
-            highlight = hit.transform;
-
-            if (highlight.CompareTag("Selectable"))
+        if(currentCamera.tag == "MainCamera") {
+            if (Physics.Raycast(ray, out hit, maxDistance))
             {
-                    if (highlight != previousHighlight && previousHighlight != null)
-                    {
-                        previousHighlight.gameObject.GetComponent<Outline>().enabled = false;
-                    }
+                highlight = hit.transform;
 
-                    if (highlight.gameObject.GetComponent<Outline>().enabled == false)
-                    {
-                        highlight.gameObject.GetComponent<Outline>().enabled = true;
-                    }
+                if (highlight.CompareTag("Selectable"))
+                {
+                        if (highlight != previousHighlight && previousHighlight != null)
+                        {
+                            previousHighlight.gameObject.GetComponent<Outline>().enabled = false;
+                        }
 
-                    previousHighlight = highlight;
+                        if (highlight.gameObject.GetComponent<Outline>().enabled == false)
+                        {
+                            highlight.gameObject.GetComponent<Outline>().enabled = true;
+                        }
+
+                        previousHighlight = highlight;
+                }
+                else if (previousHighlight != null)
+                {
+                    previousHighlight.gameObject.GetComponent<Outline>().enabled = false;
+                    previousHighlight = null;
+                }
             }
             else if (previousHighlight != null)
             {
                 previousHighlight.gameObject.GetComponent<Outline>().enabled = false;
                 previousHighlight = null;
             }
+        } else {
+            
         }
-        else if (previousHighlight != null)
+        
+
+        if (Input.GetButtonDown("Fire1"))
         {
-            previousHighlight.gameObject.GetComponent<Outline>().enabled = false;
-            previousHighlight = null;
+            if (Physics.Raycast(ray, out hit, maxDistance))
+            {
+                highlight = hit.transform;
+                if (highlight.CompareTag("CameraSelectable"))
+                {
+                    //get raycast hit object
+                    //get the object's script pressed to camera
+                    //Switch camera First Person Camera to Overhead Camera
+                    var pressedToCamera = highlight.Find("Button Camera"); 
+                    if (pressedToCamera != null && pressedToCamera != null && currentCamera != null)
+                    {
+                        Camera changedCamera = pressedToCamera.GetComponent<Camera>();
+                        currentCamera.enabled = false;
+                        currentCamera = changedCamera;
+                        changedCamera.enabled = true;
+                    }
+                    else
+                    {
+                        Debug.LogError("Camera or OverheadCamera is not assigned.");
+                    }
+                }
+            }
         }
     }
-
-    // // Selection
-    // if (Input.GetButtonDown("Fire1"))
-    // {
-    //     if (highlight)
-    //     {
-    //         if (selection != null)
-    //         {
-    //             selection.gameObject.GetComponent<Outline>().enabled = false;
-    //         }
-    //         selection = raycastHit.transform;
-    //         selection.gameObject.GetComponent<Outline>().enabled = true;
-    //         highlight = null;
-    //     }
-    //     else
-    //     {
-    //         if (selection)
-    //         {
-    //             selection.gameObject.GetComponent<Outline>().enabled = false;
-    //             selection = null;
-    //         }
-    //     }
-    // }
-
-
 }
