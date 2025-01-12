dbfb6bc91b8ba5c3fd6ab914ee0acecad0a2739e
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-01-12 02:52:19 -0500

    3rd person controller, not fully implemented

--- Project Survive/Assets/Scripts/Puzzles/Lever.cs
+++ Project Survive/Assets/Scripts/Puzzles/Lever.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
-using FishNet.Object.Synchronizing;
+// using FishNet.Object.Synchronizing;
 
 public class Lever : NetworkBehaviour
 {
